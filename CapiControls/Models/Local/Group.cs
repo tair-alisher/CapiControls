ce90c0cefa3a4ea9d1cbf2dd34b8cf6046c0157f
tair-alisher <ur.alisher@mail.ru>
2019-04-08 16:48:23 +0600

    small template changes

--- CapiControls/Models/Local/Group.cs
+++ CapiControls/Models/Local/Group.cs
@@ -7,6 +7,7 @@ namespace CapiControls.Models.Local
     {
         [Key]
         public Guid Id { get; set; }
+        [Display(Name = "Наименование")]
         public string Title { get; set; }
     }
 }
--- CapiControls/Models/Local/Questionnaire.cs
+++ CapiControls/Models/Local/Questionnaire.cs
@@ -7,9 +7,13 @@ namespace CapiControls.Models.Local
     {
         [Key]
         public Guid Id { get; set; }
+        [Display(Name = "Группа")]
         public Guid GroupId { get; set; }
+        [Display(Name = "Группа")]
         public string Group { get; set; }
+        [Display(Name = "Идентификатор")]
         public string Identifier { get; set; }
+        [Display(Name = "Наименование")]
         public string Title { get; set; }
     }
 }
