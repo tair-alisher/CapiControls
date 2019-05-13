315247f5e18cbefd752156adf1e3862e50d47654
tair-alisher <ur.alisher@mail.ru>
2019-05-13 09:19:12 +0600

    GroupService_Tests class created

---
+++ CapiControls.BLL.Tests/GroupService_Tests.cs
@@ -0,0 +1,22 @@
+﻿using CapiControls.BLL.Interfaces;
+using CapiControls.DAL.Entities;
+using System.Collections.Generic;
+
+namespace CapiControls.BLL.Tests
+{
+    public class GroupService_Tests
+    {
+        IGroupService GroupService;
+        int commits = 0;
+        List<Group> GroupsData;
+
+        public GroupService_Tests()
+        {
+            GroupsData = new List<Group>()
+            {
+
+            };
+        }
+
+    }
+}
